f814f0f8941abe058819a957d789b5800b445fbc
kbledzki <krzysztofbledzki@gmail.com>
2023-07-11 16:25:18 +0200

    add exceptions co

--- src/main/java/com/kb/RestApiCurrencyExchange/service/CurrencyExchangeService.java
+++ src/main/java/com/kb/RestApiCurrencyExchange/service/CurrencyExchangeService.java
@@ -15,11 +15,7 @@ import org.springframework.web.client.RestTemplate;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
