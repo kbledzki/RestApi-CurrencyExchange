98f44ceeb22e4c89cd68148764cd470da3373bb0
kbledzki <krzysztofbledzki@gmail.com>
2023-07-11 15:41:59 +0200

    initial commit

---
+++ src/main/java/com/kb/RestApiCurrencyExchange/RestApiCurrencyExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.kb.RestApiCurrencyExchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCurrencyExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiCurrencyExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kb/RestApiCurrencyExchange/RestApiCurrencyExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kb.RestApiCurrencyExchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCurrencyExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
